ee23ac55f0803cd00dc63fb0db2b0f2c0f80d6ef
aranorj <arano.rocio.b@gmail.com>
2022-05-26 08:41:13 -0300

    feat: add login form

--- src/app.js
+++ src/app.js
@@ -17,20 +17,29 @@ app.listen(port, ()=>{
     console.log(`Escuchando por el puerto ${port}`)
 });
 
-//defino el home vinculandolo con un html 
+//defino ñas rutas vinculandolas con un html 
+
 const homePath = path.join(__dirname, "/views/home.html")
 app.get("/",(req, res)=>{
 res.sendFile(homePath);
 })
 
-//defino el registro vinculandolo con un html 
 const registerPath = path.join(__dirname, "/views/register.html")
 app.get("/register",(req, res)=>{
 res.sendFile(registerPath);
 })
 
-//la ruta a donde se enviarán los datos
-app.post("/datos",(req, res)=>{
+const loginPath = path.join(__dirname, "/views/login.html")
+app.get("/login",(req, res)=>{
+res.sendFile(loginPath);
+})
+
+//las rutas a donde se enviarán los datos
+
+app.post("/datos-registro",(req, res)=>{
 res.send(req.body);
+})
+app.post("/datos-login",(req, res)=>{
+    res.send(req.body);
 })
 
